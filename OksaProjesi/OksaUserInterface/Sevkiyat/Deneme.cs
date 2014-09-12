071c088ea3310e9288bbafeb305fe15cec77f226
proofles <veyselsolak@gmail.com>
2014-09-12 17:43:42 +0300

    deneme form

---
+++ OksaProjesi/OksaUserInterface/Sevkiyat/Deneme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OksaUserInterface.Sevkiyat
+{
+    public partial class Deneme : Form
+    {
+        public Deneme()
+        {
+            InitializeComponent();
+        }
+    }
+}
